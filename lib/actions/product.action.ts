6821e79527cfce9b172250ae8e20abc5636dcd28
shakib <atking2240@gmail.com>
2025-08-27 10:38:43 +0600

    Added

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -19,3 +19,20 @@ import { convertToPlainObject } from "../utils";
     rating: Number(p.rating), // convert Decimal → number
   })));
 }
+
+export async function getProductBySlug(slug: string) {
+    const prisma = new PrismaClient();
+    const product = await prisma.product.findFirst({
+        where: { slug },
+    });
+    if (!product) return null;
+    return {
+        ...convertToPlainObject(product),
+        price: Number(product.price),   // convert Decimal → number
+        rating: Number(product.rating), // convert Decimal → number
+    };
+
+    // return prisma.product.findFirst({
+    //     where: { slug },
+    // });
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['fakestoreapi.com'],
+  },
 };
 
 export default nextConfig;
